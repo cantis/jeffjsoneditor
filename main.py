a8e94ba74593d12f0479e393c0c4443deb99a336
Evan Young <cantis@gmail.com>
2025-10-27 21:03:36 -0500

    Fix string quotes for consistency in main.py

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from src.app import app
 
 
 def main():
-    print("Starting Alpha Strike Group Editor...")
-    print("Navigate to http://localhost:5000 in your browser")
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    print('Starting Alpha Strike Group Editor...')
+    print('Navigate to http://localhost:5000 in your browser')
+    app.run(debug=True, host='0.0.0.0', port=5000)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
